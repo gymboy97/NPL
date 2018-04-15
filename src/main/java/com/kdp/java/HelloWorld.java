91aca39b1d4e54080b6dcf338f5b40afd8049efa
Siddharth <37174827+gymboy97@users.noreply.github.com>
2018-04-15 12:37:57 +0530

    Update HelloWorld.java

--- src/main/java/com/kdp/java/HelloWorld.java
+++ src/main/java/com/kdp/java/HelloWorld.java
@@ -2,10 +2,7 @@ package com.kdp.java;
 
 public class HelloWorld {
 
-	/**
-	 * @author Kshitij Pawar	
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Greeting obj = new Greeting();
